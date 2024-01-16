931d759570893f2c5bd5adde8e3dfdda4c008596
Gabriele Stulzer <gabriele.stulzer@gmail.com>
2024-01-16 09:01:50 +0100

    Dataset copy

---
+++ dataset/RefcocogDataset copy.py
@@ -0,0 +1,109 @@
+import json
+import pandas
+from skimage import io
+from torch.utils.data import random_split
+from typing import Sequence, Union
+
+from PIL import Image, ImageDraw
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as T
+import torch
+import numpy as np
+
+
+class RefcocogDataset(Dataset):
+    def __init__(self, base_path, split=None, transform=None, tokenization=None):
+        annotation_path = base_path + "/annotations/"
+
+        self.IMAGES_PATH = base_path + "/images/"
+        self.transform = transform
+        self.tokenization = tokenization
+
+        tmp_annotations = pandas.read_pickle(annotation_path + "refs(umd).p")
+        tmp_instances = json.load(open(annotation_path + "instances.json", "r"))
+
+        annotations_dt = pandas.DataFrame.from_records(tmp_annotations) \
+            .filter(items=["image_id", "split", "sentences", "ann_id"])
+
+        instances_dt = pandas.DataFrame.from_records(tmp_instances['annotations'])
+
+        # Add Explode to separate list-like sentences column and use them as separate samples
+        self.annotations = annotations_dt \
+            .merge(instances_dt[["id", "bbox", "area", "segmentation"]], left_on="ann_id", right_on="id") \
+            .explode('sentences', ignore_index=True) \
+            .drop(columns="id")
+
+        if split is not None:
+            self.annotations = self.__get_annotations_by_split(split.lower())
+
+    def getImage(self, sample):
+        id = sample['idx'][0].item()
+        item = self.annotations.iloc[id]
+        image = self.__getimage(item.image_id)
+
+        return image
+
+    def getSentences(self, sample):
+        id = sample['idx'][0].item()
+        item = self.annotations.iloc[id]
+
+        return self.__extract_sentences(item.sentences)
+
+    def __computeGroundTruth(self, item):
+        image = self.__getimage(item.image_id)
+        mask = Image.new("L", image.size)
+        draw = ImageDraw.Draw(mask)
+        draw.polygon(item.segmentation[0], fill="white", width=0)
+
+        mask = mask.resize((640, 640))
+
+        return self.__img_preprocess(mask)
+    
+    
+    def __img_preprocess(self, image: Image, n_px: int = 224, grid_px: int = 14):
+        resized = T.Resize(n_px, interpolation=Image.BICUBIC)(image)
+        crop = T.CenterCrop(n_px)(resized)
+
+        grid = T.Resize(grid_px, interpolation=Image.BICUBIC)(crop)
+
+        arr = torch.tensor(np.asarray(grid))
+        arr[arr <= 0.0] = 0 # make the image binary
+        arr[arr > 0.0] = 1 # make the image binary
+        return arr
+    
+    def __get_train_annotations(self):
+        return self.annotations[self.annotations.split == "train"].reset_index()
+
+    def __get_annotations_by_split(self, split):
+        return self.annotations[self.annotations.split == split].reset_index()
+
+    def __getimage(self, id):
+        return Image.open(self.IMAGES_PATH + "COCO_train2014_" + str(id).zfill(12) + ".jpg")
+
+    def __extract_sentences(self, sentence):
+        return f"{sentence['sent']}"
+
+    def __tokenize_sents(self, sentences):
+        return [self.tokenization(s) for s in sentences]
+
+    def __len__(self):
+        return self.annotations.shape[0]
+
+    def __getitem__(self, idx):
+        # Return {sample, sentence, id}, bbox
+        # Return single sentence, probably preprocess needed so we do not waste data
+        
+        item = self.annotations.iloc[idx]
+        image = self.__getimage(item.image_id).resize((640, 640))
+        sentences = self.__extract_sentences(item.sentences)
+
+        if self.transform:
+            image = self.transform(image)
+
+        if self.tokenization:
+            sentences = self.__tokenize_sents(sentences)
+
+        sample = {'idx': idx, 'image': image, 'sentences': sentences}
+
+        return sample, {'bbox': self.__bbox_image(item), 'gt': self.__computeGroundTruth(item), 'gt_refiner':self.__computeGroundTruthRefiner(item)}
+    
